c8c1f8e391628b4eaf2f6febd1c00d38f35f01c9
xTvini <142419660+xTvini@users.noreply.github.com>
2024-05-13 06:33:01 -0300

    Add files via upload

---
+++ snake/include/paineis.h
@@ -0,0 +1,16 @@
+/**
+ * paineis.h
+ * Created on May, 10th 2024
+ * Author: Vinicius Diniz 
+*/
+#ifndef PAINEIS_H
+#define PAINEIS_H
+
+#include <stdio.h> // Para printf e scanf
+#include <unistd.h> // Para sleep
+
+int painel_de_escolha_inicio();
+
+int escolha_dificuldade();
+
+#endif /* PAINEIS_H */
